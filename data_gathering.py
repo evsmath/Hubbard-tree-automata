ef6c6bba995d79add40358aa752fa22dcd1256e0
Eduardo Ventilari Sodré <eduvsodre@hotmail.com>
2026-06-12 22:25:25 -0400

    Update data_gathering.py

--- data_gathering.py
+++ data_gathering.py
@@ -17,33 +17,104 @@ import hubbard_tree_automata as hta
 
 ####
 
-def main():
+def size_of_mating_with(theta1, N):
+    # Fixed an angle theta1
+    # Prints the sizes of all the matings of theta1 with a/2**N
+
+    denom = 2 ** N
     
-    theta = Fraction(1,4)
-    
-    for a in range(1, 32):
-        M = hta.mating_dyadics(theta, Fraction(a,32)).matrix
+    for a in range(1, denom):
         
-        eigenvalues = np.linalg.eigvals(M)
+        theta2 = Fraction(a, denom)
+        A = hta.mating_dyadics(theta1, theta2)
+        size = A.size
         
-        maximum = 0
-        
-        for eigenvalue in eigenvalues:
-            if np.isreal(eigenvalue) and eigenvalue > maximum:
-                maximum = eigenvalue
-        
-        entropy = round(math.log(maximum), 8)
-        
-        print(f'Fraction: {a}/32, entropy = {entropy}')
+        print(f'Mating {theta1} with {theta2}: size = {size}')
         
     return
 
 ####
 
+def leading_eig(M):
+    # Input is a Perron-Frobenius matrix (np array)
+    # Output is the leading real eigenvalue
+    
+    eigenvalues = np.linalg.eigvals(M)
+    
+    maximum = 0
+    
+    for eigenvalue in eigenvalues:
+        if np.isreal(eigenvalue):
+            if eigenvalue.real > maximum:
+                maximum = eigenvalue
+    
+    return maximum
+
+####
+
+def entropy(M):
+    # Input is an adajcency matrix for a directed graph with loops
+    # Output is the entropy
+    
+    # since the leading eigenvalue is already real, may take its real part
+    # Otherwise python complains
+    
+    leading_eigenvalue = leading_eig(M).real
+    
+    return math.log(leading_eigenvalue)
+    
+####
+
+def entropy_of_mating_with(theta1, N):
+    # Fixed an angle theta1
+    # Prints the entropies of all the matings of theta1 with a/2**N
+
+    denom = 2 ** N
+    
+    for a in range(1, denom):
+        
+        theta2 = Fraction(a, denom)
+        A = hta.mating_dyadics(theta1, theta2)
+        ent = round(entropy(A.matrix), 8)
+        
+        print(f'Mating {theta1} with {theta2}: entropy = {ent}')
+        
+    return
+
+####
+
+def plot_zero_entropy_angles(theta1, N):
+    # Fixed an angle theta1
+    # Outputs a plot of all dyadic angleswith denominator 2**N whose mating with theta1 has zero entropy
+    
+    denom = 2 ** N
+    
+    dots_x_red = []
+    dots_y_red = []
+    
+    for a in range(1, denom):
+        
+        theta2 = Fraction(a, denom)
+        A = hta.mating_dyadics(theta1, theta2)
+        h = entropy(A.matrix)
+        
+        if h == 0:
+            dots_x_red.append(math.cos(theta2 * math.tau))
+            dots_y_red.append(math.sin(theta2 * math.tau))
+            
+    plt.plot(dots_x_red, dots_y_red, ',')
+    plt.gca().set_aspect("equal")
+    plt.xlim(-1.2, 1.2)
+    plt.ylim(-1.2, 1.2)
+    plt.suptitle(f"Zero entropy on matings with {theta1}")
+    plt.show()
+
+    
+    return
+
 def main1():
     
-    theta = Fraction(1,4)
-    
+    theta1 = Fraction(1,4)
     M = ''
     
     for n in range(6, 20):
@@ -51,7 +122,9 @@ def main1():
         l = []
     
         for a in range(1, 2**n):
-            A = hta.mating_dyadics(theta, Fraction(a,2**n))
+            
+            theta2 = Fraction(a, 2**n)
+            A = hta.mating_dyadics(theta1, theta2)
             
             if A.size == 4 and A.size != 2:
                 l.append(a)
